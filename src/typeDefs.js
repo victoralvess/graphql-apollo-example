932c657e7c55883e2735c9f0045e53d1927f1ceb
Victor Alves <vsilva.programador@gmail.com>
2018-04-03 19:14:37 -0300

    Update typeDefs.js

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -4,11 +4,18 @@ module.exports = `
 		completedTodos: [Todo]
 		activeTodos: [Todo]
 		todo(id: ID!): Todo
+		user(id: ID!): User
 	}
 
 	type Todo {
 		id: ID!
 		task: String!
 		complete: Boolean
+		user: User!
+	}
+
+	type User {
+		id: ID!
+		username: String!
 	}
 `;
